99c27c1f71c73776171cb2d0874debf50077ecaa
rmugabe <sadsanta@gmail.com>
2020-10-10 13:37:34 +0300

    SwiftIdentifier license header

--- Sources/SUR/Utils/SwiftIdentifier.swift
+++ Sources/SUR/Utils/SwiftIdentifier.swift
@@ -1,3 +1,11 @@
+//
+//  SwiftIdentifier.swift
+//  R.swift
+//
+//  Created by Mathijs Kadijk on 11-12-15.
+//  From: https://github.com/mac-cain13/R.swift
+//  License: MIT License
+//
 
 import Foundation
 
