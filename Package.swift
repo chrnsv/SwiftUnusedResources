d2d4f53756f6595a99843608404ddffa4f11b30b
Alexander Chernousov <chrnsv@gmail.com>
2024-07-19 18:08:14 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .executable(name: "sur", targets: ["SUR"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax", from: "510.0.2"),
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
         .package(url: "https://github.com/Bouke/Glob.git", from: "1.0.5"),
         .package(url: "https://github.com/tuist/XcodeProj.git", from: "8.22.0"),
