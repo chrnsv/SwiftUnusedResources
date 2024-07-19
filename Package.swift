e56da81d046270ab59b97219337c29d0efa939b7
Alexander Chernousov <chrnsv@gmail.com>
2024-07-19 18:07:21 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
         .package(url: "https://github.com/Bouke/Glob.git", from: "1.0.5"),
-        .package(url: "https://github.com/tuist/XcodeProj.git", from: "8.15.0"),
+        .package(url: "https://github.com/tuist/XcodeProj.git", from: "8.22.0"),
         .package(url: "https://github.com/IBDecodable/IBDecodable.git", branch: "master"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.0.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
-        .package(url: "https://github.com/realm/SwiftLint", from: "0.54.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.55.1"),
     ],
     targets: [
         .executableTarget(
@@ -32,7 +32,7 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
              ],
             plugins: [
-                .plugin(name: "SwiftLintPlugin", package: "SwiftLint")
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
             ]
         ),
     ],
